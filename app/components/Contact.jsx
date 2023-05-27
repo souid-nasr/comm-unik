9749cf9ca90f94d28b7ce795fd4546c69877b969
Nasr Souid <91200028+souid-nasr@users.noreply.github.com>
2023-05-27 05:15:41 +0100

    Update contact.jsx

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -16,8 +16,7 @@ function Contact() {
       <div className="map">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
-          frameborder="0"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
 
@@ -59,9 +58,6 @@ function Contact() {
 
           <div className="col-lg-8">
             <form
-              action="forms/contact.php"
-              method="post"
-              role="form"
               className="php-email-form"
             >
               <div className="row">
